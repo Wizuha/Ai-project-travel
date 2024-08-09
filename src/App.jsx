57c61d5a1a9ef2a24bc7eac99ef578ab85ae75c1
Wizuha <williams.wandji@gmail.com>
2024-08-09 17:29:30 +0200

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from '@mui/material'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <h1>Hello World</h1>
+      <Button>Appuie ici</Button>
     </>
   )
 }
---
+++ src/create-trip/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CreateTrip() {
+  return (
+    <div><h1>CreateTrip</h1></div>
+  )
+}
+
+export default CreateTrip
